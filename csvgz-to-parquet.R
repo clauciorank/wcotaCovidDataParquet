a090881ca4908153b61f8a47cf9f2dc1123e5ebb
Claucio Antonio Rank Filho <claucio.filho@hitechnologies.com.br>
2022-03-11 15:31:29 -0300

    data-changes

--- csvgz-to-parquet.R
+++ csvgz-to-parquet.R
@@ -3,6 +3,12 @@ temp <- tempfile()
 
 download.file('https://github.com/wcota/covid19br/raw/master/cases-brazil-cities-time_changesOnly.csv.gz', temp)
 
-data <- readr::read_csv(temp)
+data <- readr::read_csv(temp) |>
+                       mutate(city = case_when(
+                              grepl(pattern = 'CASO SEM LOCALIZAÇÃO DEFINIDA', city) ~ 'Todas',
+                              TRUE ~ city)) |>
+                      mutate(state = case_when(
+                              grepl(pattern = 'TOTAL', state) ~ 'Todos',
+                              TRUE ~ state))
 
 arrow::write_parquet(data, 'cases-brazil-cities-time-changesonly.parquet')
